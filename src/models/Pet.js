9188e7ca82e029fc755ec7bdd84b7007077546b7
victor-hugo-ss <victorabreu2512@gmail.com>
2026-04-03 16:45:20 -0300

    feat: criação de schema de Pet

---
+++ src/models/Pet.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const Pet = mongoose.model(
+    'Pet',
+    new Schema(
+        {
+            name: {
+                type: String,
+                required: true,
+            },
+            age: {
+                type: Number,
+                required: true,
+            },
+            weight: {
+                type: Number,
+                required: true,
+            },
+            color: {
+                type: String,
+                required: true,
+            },
+            images: {
+                type: [String],
+                required: true,
+            },
+            available: {
+                type: Boolean,
+            },
+            user: Object,
+            adopter: Object,
+        },
+        { timestamps: true }
+    )
+);
+
+export default Pet;
