fa1484709d55f4a33176f51bdac3841b29216058
victor-hugo-ss <victorabreu2512@gmail.com>
2026-04-03 13:48:27 -0300

    chore: configuração inicial do projeto

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+// Importar Routes
+
+const app = express();
+
+// Midlewares
+app.use(express.json());
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
+
+app.use(express.static("public"));
+
+//Routes
+
+export default app;
