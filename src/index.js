261543757111ca9ed8411fb5634cec50b1490ec8
victor-hugo-ss <victorabreu2512@gmail.com>
2026-04-03 16:15:19 -0300

    feat: conexão com o MongoBD utilizando Mongoose

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+import "dotenv/config";
 import app from "./app.js";
+import { connectDB } from "./config/db.js";
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta: ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
